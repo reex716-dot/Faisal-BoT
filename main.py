634f3674a83fddc6ab050d9bce0640758fd4a961
reex716-dot <reex716@gmail.com>
2026-02-24 03:04:46 +0300

    Change BOT_TOKEN in main.py
    Updated the BOT_TOKEN for the bot configuration.

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from pyrogram.errors import FloodWait, RPCError
 # --- إعدادات البيانات الخاصة بك ---
 API_ID = 34257542
 API_HASH = "614a1b5c5b712ac6de5530d5c571c42a"
-BOT_TOKEN = "8576422165:AAFS1w9OrSoq5yLISbfNw60VilfHpdBqmgY"
+BOT_TOKEN = "8514118433:AAEvB4nWdb6qkMUq8WsY-SWzgmQmxOmyMzQ"
 MY_USER_ID = 1486879970 
 
 # ملفات البيانات
